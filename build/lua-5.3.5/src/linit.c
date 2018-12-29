90685d906e7181b4931115112bbdd07f88f758c0
hangangliu <2980172@163.com>
2018-12-29 13:55:30 +0800

    添加ripidjson

--- build/lua-5.3.5/src/linit.c
+++ build/lua-5.3.5/src/linit.c
@@ -50,6 +50,10 @@ static const luaL_Reg loadedlibs[] = {
   {LUA_MATHLIBNAME, luaopen_math},
   {LUA_UTF8LIBNAME, luaopen_utf8},
   {LUA_DBLIBNAME, luaopen_debug},
+  { LUA_SOCKETLIBNAME, luaopen_socket_core },
+  { LUA_LFSLIBNAME, luaopen_lfs },
+  { LUA_PBLIBNAME, luaopen_pb },
+  { LUA_PBLIBNAME, luaopen_rapidjson },
 #if defined(LUA_COMPAT_BITLIB)
   {LUA_BITLIBNAME, luaopen_bit32},
 #endif
